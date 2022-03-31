5e0311229491d25d68737d034a5ff0aa7fea9ea6
weeratan <57850779+weeratan@users.noreply.github.com>
2022-03-31 14:57:40 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
